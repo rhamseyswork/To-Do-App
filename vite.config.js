ee1a4d160c5838dd1c24b5b807764e162b7eb198
rhamseyswork <129332988+rhamseyswork@users.noreply.github.com>
2024-02-12 14:21:27 -0800

    test fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: 'https://rhamseyswork.github.io/To-Do-App/', // Adjust this based on your GitHub Pages URL path
+  plugins: [react()]
+});
